e77a55e4894bc8618690cdaee9625991f293da3d
catab1e <tilektursunbaiuulu@gmail.com>
2022-10-29 19:57:01 +0600

    new features

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,41 +1,47 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import AdminPage from './pages/AdminPage';
-import CardsPages from './pages/CardsPages';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
+import AdminPage from "./pages/AdminPage";
+import CardsPages from "./pages/CardsPages";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import PricePage from "./pages/PricePage";
 
 const MainRoutes = () => {
-    const ALL_ROUTES = [
-        {
-            link: "/",
-            element: <HomePage/>,
-            id: 1,
-        },
-        {
-            link: "/cards",
-            element: <CardsPages/>,
-            id: 2,
-        },
-        {
-            link: "/*",
-            element: <NotFoundPage/>,
-            id: 3,
-        },
-        {
-            link: "/admin123",
-            element: <AdminPage/>,
-            id: 4,
-        },
-    ];
+  const ALL_ROUTES = [
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: "/cards",
+      element: <CardsPages />,
+      id: 2,
+    },
+    {
+      link: "/price",
+      element: <PricePage />,
+      id: 3,
+    },
+    {
+      link: "/*",
+      element: <NotFoundPage />,
+      id: 4,
+    },
+    {
+      link: "/admin123",
+      element: <AdminPage />,
+      id: 5,
+    },
+  ];
 
-    return (
-        <Routes>
-        {ALL_ROUTES.map((item) => (
-            <Route path = {item.link} element = {item.element} key = {item.id}/>
-        ))}
-        </Routes>
-    );
+  return (
+    <Routes>
+      {ALL_ROUTES.map((item) => (
+        <Route path={item.link} element={item.element} key={item.id} />
+      ))}
+    </Routes>
+  );
 };
 
 export default MainRoutes;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
         <div className='footer'>
             <div className='footer__inner'>
                 <div className='inner-left'>
-                    <a className='astyle' href="/">Default</a>
-                    <a className='astyle' href="/">Default</a>
-                    <a className='astyle' href="/">Default</a>
-                    <a className='astyle' href="/">Default</a>
+                    <a className='astyle' href="/">Часто задаваемые вопросы</a>
+                    <a className='astyle' href="/">Контакты</a>
+                    <a className='astyle' href="/">Цены</a>
+                    <a className='astyle' href="/">Что-то</a>
                 </div>
                 <div className='inner-right'>
                     <a className='astyle' href="/">Default</a>
